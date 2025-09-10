ade44d7268dd802caca674f82ea4a14345cc3758
Dusan Prica <prica.dusan16@gmail.com>
2025-09-10 09:47:13 +0200

    next.config uploaded

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-      output: "export",
- images: {
-  unoptimized: true,
+  output: 'export',
   trailingSlash: true,
   assetPrefix: '/',
-},
-};
-
-
+  images: {
+    unoptimized: true,
+  },
+}
 
 export default nextConfig;
